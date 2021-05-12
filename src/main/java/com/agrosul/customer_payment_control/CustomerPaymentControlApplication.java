4122dee6786ef5b547dd2c008a2f3aa588a97a3f
Smaniotto <ghsmaniotto@inf.ufpel.edu.br>
2021-05-12 08:14:47 -0300

    Starting project

---
+++ src/main/java/com/agrosul/customer_payment_control/CustomerPaymentControlApplication.java
@@ -0,0 +1,13 @@
+package com.agrosul.customer_payment_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerPaymentControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerPaymentControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agrosul/customer_payment_control/CustomerPaymentControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agrosul.customer_payment_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerPaymentControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
