19361187348a40578d500d2cfd0a11f0e501b8e3
Smaniotto <ghsmaniotto@inf.ufpel.edu.br>
2021-05-19 07:37:56 -0300

    Add phone fields validation

--- src/main/java/com/agrosul/customer_payment_control/domain/Phone.java
+++ src/main/java/com/agrosul/customer_payment_control/domain/Phone.java
@@ -1,9 +1,31 @@
 package com.agrosul.customer_payment_control.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
+@Entity
+@Table(name = "phones")
 public class Phone extends BaseEntity {
+  @NotNull(message = "Country code must exists")
+  @Length(min = 2, max = 2)
+  @Column(name = "country_code", nullable = false, length = 2)
   private String countryCode;
+
+  @NotNull(message = "Area code must exists")
+  @Length(min = 2, max = 2)
+  @Column(name = "area_code", nullable = false, length = 2)
   private String areaCode;
+
+  @NotNull(message = "Number must exists")
+  @Length(min = 8, max = 9)
+  @Column(name = "number", nullable = false, length = 9)
   private String number;
+
+  public Phone(){}
 
   public Phone(String countryCode, String areaCode, String number) {
     this.countryCode = countryCode;
