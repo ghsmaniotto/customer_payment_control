74871b81f339245a4435af7f9cd7d03faeddf686
Smaniotto <ghsmaniotto@inf.ufpel.edu.br>
2021-05-19 07:37:37 -0300

    Update base entity to use long ID instead of UUID

--- src/main/java/com/agrosul/customer_payment_control/domain/BaseEntity.java
+++ src/main/java/com/agrosul/customer_payment_control/domain/BaseEntity.java
@@ -1,16 +1,25 @@
 package com.agrosul.customer_payment_control.domain;
 
 import java.beans.Transient;
-import java.io.Serializable;
 
-public class BaseEntity implements Serializable {
-  private String id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.SequenceGenerator;
 
-  public String getId() {
+@MappedSuperclass
+public class BaseEntity {
+  @Id
+  @SequenceGenerator(name = "base_entity_sequence", sequenceName = "base_entity_sequence", allocationSize = 1)
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "base_entity_sequence")
+  private Long id;
+
+  public Long getId() {
     return this.id;
   }
 
-  public void setId(String id) {
+  public void setId(Long id) {
     this.id = id;
   }
 
