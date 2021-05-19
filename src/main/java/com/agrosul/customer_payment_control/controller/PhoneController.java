6b5f00fa4371122e00766736ea0d30d885a5c48a
Smaniotto <ghsmaniotto@inf.ufpel.edu.br>
2021-05-19 07:39:18 -0300

    Add post method to phone routes

--- src/main/java/com/agrosul/customer_payment_control/controller/PhoneController.java
+++ src/main/java/com/agrosul/customer_payment_control/controller/PhoneController.java
@@ -2,13 +2,19 @@ package com.agrosul.customer_payment_control.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import com.agrosul.customer_payment_control.domain.Phone;
 import com.agrosul.customer_payment_control.service.PhoneService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping(path = "api/v1/phones")
@@ -25,5 +31,10 @@ public class PhoneController {
   @GetMapping(path="{phoneId}")
   public Phone getPhone(@PathVariable("phoneId") Long phoneId) {
     return phoneService.getPhone(phoneId);
+  }
+
+  @PostMapping
+  public Phone addPhone(@Valid @RequestBody Phone phone) {
+      return phoneService.createPhone(phone);
   }
 }
