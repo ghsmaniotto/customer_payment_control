c6512aaaa6d01d7044f72cffda94e2ea88f0a3f4
Smaniotto <ghsmaniotto@inf.ufpel.edu.br>
2021-05-27 07:39:18 -0300

    Add test for phone mapper

---
+++ src/test/java/com/agrosul/customer_payment_control/dto/phone/PhoneMapperTest.java
@@ -0,0 +1,92 @@
+package com.agrosul.customer_payment_control.dto.phone;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import com.agrosul.customer_payment_control.domain.Phone;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Phone Mapper tests")
+public class PhoneMapperTest {
+  Phone phone = build_phone();
+  PhoneDTO phoneDTO = build_phoneDTO();
+  
+  @Nested
+  @DisplayName("fromPhone method")
+  class FromPhone {
+    @Test
+    @DisplayName("must convert entity phone to dto")
+    void mustConvertPhoneEntityToDTO(){
+      PhoneDTO dtoMapped = new PhoneMapper().fromPhone(phone);
+      
+      assertEquals(dtoMapped.getId(), phone.getId());
+      assertEquals(dtoMapped.getCountryCode(), phone.getCountryCode());
+      assertEquals(dtoMapped.getAreaCode(), phone.getAreaCode());
+      assertEquals(dtoMapped.getNumber(), phone.getNumber());
+    }
+  }
+
+  @Nested
+  @DisplayName("To phone method")
+  class ToPhone {
+    @Test
+    @DisplayName("must convert phone dto to entity")
+    void mustConvertPhoneDTOToEntity() {
+      Phone phoneMapped = new PhoneMapper().toPhone(phoneDTO);
+
+      assertEquals(phoneMapped.getId(), phoneDTO.getId());
+      assertEquals(phoneMapped.getCountryCode(), phoneDTO.getCountryCode());
+      assertEquals(phoneMapped.getAreaCode(), phoneDTO.getAreaCode());
+      assertEquals(phoneMapped.getNumber(), phoneDTO.getNumber());
+    }
+  }
+
+  @Nested
+  @DisplayName("updatePhoneToDTO method")
+  class updatePhoneToDTO {
+    @Test
+    @DisplayName("must update phone entity based on DTO attr")
+    void mustConvertPhoneDTOToEntity() {
+      new PhoneMapper().updatePhoneFromDto(phoneDTO, phone);;
+
+      assertEquals(phone.getId(), phoneDTO.getId());
+      assertEquals(phone.getCountryCode(), phoneDTO.getCountryCode());
+      assertEquals(phone.getAreaCode(), phoneDTO.getAreaCode());
+      assertEquals(phone.getNumber(), phoneDTO.getNumber());
+    }
+
+    @Test
+    @DisplayName("must skip null attributes")
+    void mustSkipNullAttributes(){
+      String areaCode = phone.getAreaCode();
+      phoneDTO.setAreaCode(null);
+      
+      new PhoneMapper().updatePhoneFromDto(phoneDTO, phone);
+
+      assertNotEquals(phone.getAreaCode(), phoneDTO.getAreaCode());
+      assertEquals(phone.getAreaCode(), areaCode);
+    }
+  }
+
+  Phone build_phone(){
+    Phone phone = new Phone("55", "51", "997112290");
+    phone.setId(1L);
+    return phone;
+  }
+  
+  PhoneDTO build_phoneDTO() {
+    PhoneDTO dto = new PhoneDTO("55", "51", "997112290");
+    dto.setId(2L);
+    return dto;
+  }
+
+  @BeforeEach
+  void init() {
+    this.phone = build_phone();
+    this.phoneDTO = build_phoneDTO();
+  }
+}
