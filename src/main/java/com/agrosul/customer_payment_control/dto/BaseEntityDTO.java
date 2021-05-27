0baf01bd6938d75f6962a6b1dbe9a10e8f21bc2f
Smaniotto <ghsmaniotto@inf.ufpel.edu.br>
2021-05-27 07:38:33 -0300

    Add base entity dto class

---
+++ src/main/java/com/agrosul/customer_payment_control/dto/BaseEntityDTO.java
@@ -0,0 +1,13 @@
+package com.agrosul.customer_payment_control.dto;
+
+public class BaseEntityDTO {
+  private Long id;
+
+  public Long getId() {
+    return this.id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+}
