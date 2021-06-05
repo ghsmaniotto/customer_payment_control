7bfde11f0a441a5f31e13da6d50f7adeb3ef0a04
Smaniotto <ghsmaniotto@inf.ufpel.edu.br>
2021-06-05 09:43:44 -0300

    Add customer domain model

---
+++ src/main/java/com/agrosul/customer_payment_control/domain/Customer.java
@@ -0,0 +1,66 @@
+package com.agrosul.customer_payment_control.domain;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customers")
+public class Customer extends BaseEntity {
+  private String firstName;
+  private String lastName;
+  private String email;
+  private String cpf;
+
+  public Customer() {
+  }
+
+  public Customer(String firstName, String lastName, String email, String cpf) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.cpf = cpf;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+
+  public List<Phone> getPhones() {
+    return phones;
+  }
+
+  public void setPhones(List<Phone> phones) {
+    this.phones = phones;
+  }
+}
