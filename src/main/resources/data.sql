bb7cef653cd2152e74cdcce0ea09d53859fdb233
Smaniotto <ghsmaniotto@inf.ufpel.edu.br>
2021-06-06 19:48:28 -0300

    Delete data.sql file

--- src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS phones;
-
-CREATE TABLE phones (
-  id INTEGER,
-  country_code VARCHAR NOT NULL,
-  area_code VARCHAR NOT NULL,
-  number VARCHAR NOT NULL,
-  PRIMARY KEY(id)
-);
